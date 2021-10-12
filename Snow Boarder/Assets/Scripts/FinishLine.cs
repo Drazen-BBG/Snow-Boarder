383b0c2f062d4afb61a3642cbb585c5a10d74ad6
Drazen Drinic <drazendrinic@hotmail.com>
2021-10-12 21:56:21 +0200

    Adding particle effects

--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1.0f;
+    [SerializeField] ParticleSystem finishEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
